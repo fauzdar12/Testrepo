fe875d7f5ad2c6fb664b190227508d6421d4fd5a
lenovo <lenovo@DESKTOP-LQ6E6NI>
2023-07-24 20:30:11 +0530

    addin new project in eclipse

---
+++ My_maven/src/main/java/testngjavamaven/Firstmaventestng.java
@@ -0,0 +1,12 @@
+package testngjavamaven;
+
+import org.junit.Test;
+
+public class Firstmaventestng {
+
+@Test
+public void f()
+{
+	System.out.println("Hello");//HERE I LEARNED ABOUT JUNITS IN MAVEN 
+}
+}
