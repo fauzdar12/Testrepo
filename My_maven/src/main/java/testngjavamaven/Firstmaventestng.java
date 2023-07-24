881ffc7150fe07c9db1944090e2c449421a5c5b8
lenovo <lenovo@DESKTOP-LQ6E6NI>
2023-07-24 21:33:20 +0530

    deleted modified data in projects

--- My_maven/src/main/java/testngjavamaven/Firstmaventestng.java
+++ My_maven/src/main/java/testngjavamaven/Firstmaventestng.java
@@ -8,8 +8,6 @@ public class Firstmaventestng {
 public void f()
 {
 	System.out.println("Hello");//HERE I LEARNED ABOUT JUNITS IN MAVEN 
-	System.out.println("Hello");
-	System.out.println("Hello");
-	System.out.println("Hello");
+	
 }
 }
